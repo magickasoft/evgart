ce5d2b040debfe43714c3ba111040cd5bd1dd6cf
Evgeny Shmakov <sjs-master@yandex.ru>
2024-04-22 10:24:53 +0700

    fix: fix

--- apps/root/src/utils/get.ts
+++ apps/root/src/utils/get.ts
@@ -1,9 +1,9 @@
 export const get = (obj: any, path: any, defaultValue = undefined) => {
-  const travel = (regexp: any) =>
+  const fn = (regexp: any) =>
     String.prototype.split
       .call(path, regexp)
       .filter(Boolean)
       .reduce((res, key) => (res !== null && res !== undefined ? res[key] : res), obj);
-  const result = travel(/[,[\]]+?/) || travel(/[,[\].]+?/);
+  const result = fn(/[,[\]]+?/) || fn(/[,[\].]+?/);
   return result === undefined || result === obj ? defaultValue : result;
 };
