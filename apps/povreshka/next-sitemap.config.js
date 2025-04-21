d89f0628f881579c98f6db40065edcb5e2ad6829
Evgeny Shmakov <sjs-master@yandex.ru>
2025-04-21 21:29:59 +0700

    sitemap fixes

--- apps/povreshka/next-sitemap.config.js
+++ apps/povreshka/next-sitemap.config.js
@@ -1,27 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://localhost:3000',
-  changefreq: 'daily',
-  priority: 0.7,
-  sitemapSize: 5000,
+  siteUrl: 'https://povreshka.vercel.app',
+  sitemapSize: 7000,
   generateRobotsTxt: true,
-  exclude: [],
-  alternateRefs: [],
-  // Default transformation function
-  transform: async (config, path) => {
-    return {
-      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
-      changefreq: config.changefreq,
-      priority: config.priority,
-      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-      alternateRefs: config.alternateRefs ?? [],
-    }
-  },
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
         allow: '/',
+        disallow: ['/api/*', '/_next/*'],
+        crawlDelay: 1,
       },
     ],
     additionalSitemaps: [],
