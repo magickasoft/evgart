bd71e49aa8b03b5f04edee485ebbc8815c60cb94
Evgeny Shmakov <sjs-master@yandex.ru>
2025-04-21 19:35:32 +0700

    Update hello.ts

--- apps/povreshka/src/pages/api/hello.ts
+++ apps/povreshka/src/pages/api/hello.ts
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function (req: any, res: any) {
-  res.status(200).json({ name: 'John Doe' })
+const apiHandler = async (_: NextApiRequest, res: NextApiResponse) => {
+  try {
+    res.status(200).json({ name: 'John Doe' })
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
 }
+
+export default apiHandler
