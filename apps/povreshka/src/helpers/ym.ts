6cce9d2db7a83a0bd608ee2f99c9dae6ff904af8
Evgeny Shmakov <sjs-master@yandex.ru>
2025-05-11 20:57:20 +0700

    fix lint

--- apps/povreshka/src/helpers/ym.ts
+++ apps/povreshka/src/helpers/ym.ts
@@ -1,7 +1,8 @@
 // @ts-ignore
 import withYM from 'next-ym'
-import { YANDEX_METRICA_ID } from '../process.env/YANDEX_METRICA_ID'
+
 import { isProduction } from '../process.env/NODE_ENV'
+import { YANDEX_METRICA_ID } from '../process.env/YANDEX_METRICA_ID'
 
 export const reachGoal = (name: string) => {
   // @ts-ignore
